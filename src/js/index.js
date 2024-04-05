e10636ce110e447be656987bf9cd8e4e2b0dd53d
RobPayot <robpayot@gmail.com>
2024-04-05 21:03:33 +0200

    fix: deouble tap

--- src/js/index.js
+++ src/js/index.js
@@ -1,7 +1,16 @@
 import Game from '@/js/components/Game'
-
 ;(() => {
   // component
   const el = document.querySelector('[data-game]')
   new Game(el)
+
+  // prevent double tap zoom
+  document.querySelectorAll('*').forEach((e) => {
+    e.style['touch-action'] = 'manipulation'
+  }),
+    new MutationObserver((e) => {
+      e.forEach(function (e) {
+        for (let o = 0; o < e.addedNodes.length; o++) e.addedNodes[o].style['touch-action'] = 'manipulation'
+      })
+    }).observe(document.body, { childList: !0, subtree: !0 })
 })()
